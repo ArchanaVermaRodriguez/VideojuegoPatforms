e69416071f8528f2fdc2309e9e055ecb65630c6d
ArchanaVerma <A01335895@itesm.mx>
2020-09-17 18:05:59 -0500

    boost item

--- VideojuegoPlatforms/Assets/Scripts/PlayerMovements.cs
+++ VideojuegoPlatforms/Assets/Scripts/PlayerMovements.cs
@@ -7,10 +7,13 @@ public class PlayerMovements : MonoBehaviour
 
 
     [Header ("Movement")]
-    public float Speed;
+    private float Speed;
     public float jumpForce;
     public float moveInput;
+    public static int direction=1;
+    private float originalXScale;
     public float xVelocity;
+    private bool moving;
 
     [Header("Unity Components")]
     public Rigidbody2D fisicas;
@@ -23,14 +26,22 @@ public class PlayerMovements : MonoBehaviour
     private int extraJumps=1;// de aquí se resta el valor de extraJumpsValues
     public int extraJumpsValue=1;// valor de los saltos extras 
   
+    [Header("Poder")]
+    private float boostTimer;
+    private bool boosting;
 
     private Rigidbody2D rb;
  
     void Start()
     {
+        Speed = 5;
+        moving = false;
         extraJumps= extraJumpsValue;
         rb= GetComponent<Rigidbody2D>();
         fisicas= GetComponent<Rigidbody2D>();    
+        originalXScale = transform.localScale.x;
+        boostTimer = 0;
+        boosting = false;
     }
 
     // Update is called once per frame
@@ -43,6 +54,15 @@ public class PlayerMovements : MonoBehaviour
         fisicas.velocity= new Vector2(xVelocity, fisicas.velocity.y);    
         
     }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Poder"){
+            boosting = true;
+            Destroy(other.gameObject);
+            Speed = 25;
+        }
+    }
+
     void Update(){
         if(isGrounded){
             extraJumps=extraJumpsValue;
@@ -57,6 +77,19 @@ public class PlayerMovements : MonoBehaviour
             rb.velocity= Vector2.up*jumpForce;
             extraJumps--;
         } 
+
+        if(moving){
+            this.transform.Translate(new Vector3(Time.deltaTime * Speed, 0, 0));
+        }
+
+        if(boosting){
+            boostTimer += Time.deltaTime;
+            if(boostTimer >= 3){
+                Speed = 5;
+                boostTimer = 0;
+                boosting = false;
+            }
+        }
 
     }
 
